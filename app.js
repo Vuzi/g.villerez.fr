91065c2b35f43efdbf56477ef15bb4522e52fad4
Vuzi <vuzi@free.fr>
2016-03-13 23:13:42 +0100

    Error status in prod

--- app.js
+++ app.js
@@ -54,7 +54,7 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error.html', {
     message: err.message,
-    error: {}
+    error: { status : err.status }
   });
 });
 
